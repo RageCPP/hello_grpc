8b7f463b6b11de30dd27951dfa45fe4e87a0c2de
ragecpp <mathematicscpu@gmail.com>
2023-08-15 15:28:26 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,59 @@
+#include <absl/base/attributes.h>
+#include <absl/flags/internal/flag.h>
+#include <algorithm>
+#include <cstdint>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/support/status.h>
 #include <iostream>
+#include <memory>
+#include <ostream>
+#include <string>
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "hello.grpc.pb.h"
+#include "hello.pb.h"
 
-int main() {
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using people::Meet;
+using people::HelloRequest;
+using people::HelloReply;
+
+ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
+
+class MeetServiceImpl final: public Meet::Service {
+  Status SayHello(ServerContext* context, const HelloRequest* req, HelloReply* rep) {
+    std::string prefix {"Hello "};
+    rep->set_name(prefix + req->name());
+    return Status::OK;
+  }
+};
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  MeetServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  RunServer(absl::GetFlag(FLAGS_port));
   return 0;
 }
