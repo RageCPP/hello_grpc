278aaa9c9f36fc4c2cab6279afbbbb505573c5c3
ragecpp <mathematicscpu@gmail.com>
2023-08-29 15:26:59 +0800

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 exec_path="$(pwd)"
 build_path="$exec_path/build"
+build_release_path="$build_path/release"
+build_debug_path=""
 grpc_path="$exec_path/grpc"
 
 clear() {
@@ -9,11 +11,12 @@ clear() {
   rm -rf "$grpc_path"
   mkdir "$build_path"
   mkdir "$grpc_path"
+  mkdir "$build_release_path"
 }
 
 generate_build() {
   clear
-  cmake -B "${build_path}" -S "${exec_path}"
+  cmake -B "${build_release_path}" -S "${exec_path}" --preset=x86_64-linux-g++-Release
 }
 
 grpc_lib_build() {
@@ -21,12 +24,17 @@ grpc_lib_build() {
 }
 
 grpc_lite_lib_build() {
-  cmake --build "${build_path}" --target grpc_lite_lib -- -j"$(nproc)"
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/ragecpp/hey-cpp/ubuntu-22-04/protobuf_install/share/lib:/home/ragecpp/hey-cpp/ubuntu-22-04/grpc_install/share/lib";
+  cmake --build "${build_release_path}" --target grpc_lite_lib --preset=release -- -j"$(nproc)"
 }
 
 
 cli_build() {
   cmake --build "${build_path}" --target cli -- -j"$(nproc)"
+}
+
+cli_lite_build() {
+  cmake --build "${build_path}" --target cli_lite --preset=release -- -j"$(nproc)"
 }
 
 local_server_build() {
@@ -54,6 +62,9 @@ main() {
     --cli)
       cli_build
       ;;
+    --cli_lite)
+      cli_lite_build
+      ;;
     --local_server)
       local_server_build
       ;;
@@ -61,6 +72,8 @@ main() {
       remote_server_build
       ;;
     *)
+      echo "not support command"
+      exit 1
       ;;
   esac
 }
